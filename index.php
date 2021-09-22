540847dc74f8fdc87256d9db6ebbd5b00faceb4c
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-22 17:11:26 +0200

    Reading header and shows first product

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
 use OrbitaDigital\OdBydemes\Bydemes;
+use OrbitaDigital\OdBydemes\Csv;
 
-if(!defined('_PS_VERSION_')){
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (!defined('_PS_VERSION_')) {
     require_once '../../config/config.inc.php';
     require_once '../../init.php';
 }
 include('src/Bydemes.php');
 //el stock disponible es: quantity en ps_stock_available. Requiere del id_product (de ps_product)
 // el id_stock_available deberia sumarse si el id_product es el mismo
-var_dump(Bydemes::seeStock());
+var_dump(Bydemes::seeStock());$file = 'rates/CL131545_product_list.csv';
+$reader = new Csv(['id','referencia','Model','Brand','Stock','activo','PVP','Description','Short description','Title','Category','Family','SubFamily','Compatible products','imageURL','EAN','length','width','height','volume','weight','Product URL']);
+$reader->process([$file]);
+
