584b3c1b83b6a2b0860e425583342d9acaf278c4
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-22 19:21:10 +0200

    added csv reader to see first row

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ if (!defined('_PS_VERSION_')) {
     require_once '../../config/config.inc.php';
     require_once '../../init.php';
 }
-include('src/Bydemes.php');
-//el stock disponible es: quantity en ps_stock_available. Requiere del id_product (de ps_product)
-// el id_stock_available deberia sumarse si el id_product es el mismo
-var_dump(Bydemes::seeStock());$file = 'rates/CL131545_product_list.csv';
+
+$file = 'rates/CL131545_product_list.csv';
 $reader = new Csv(['id','referencia','Model','Brand','Stock','activo','PVP','Description','Short description','Title','Category','Family','SubFamily','Compatible products','imageURL','EAN','length','width','height','volume','weight','Product URL']);
-$reader->process([$file]);
+$first = $reader->process([1 => $file]);
+var_dump($first);
+$bydemes_products = Bydemes::getBydemesProducts();
 
