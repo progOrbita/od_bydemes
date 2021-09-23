33c965d4b4bebfc5de5ba9acd0a8f5d6145202b1
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-23 16:13:03 +0200

    constructor of Csv to file,parse_header,delimitator and lenght

--- index.php
+++ index.php
@@ -10,9 +10,14 @@ if (!defined('_PS_VERSION_')) {
     require_once '../../init.php';
 }
 
-$file = 'rates/CL131545_product_list.csv';
-$reader = new Csv(['id','referencia','Model','Brand','Stock','activo','PVP','Description','Short description','Title','Category','Family','SubFamily','Compatible products','imageURL','EAN','length','width','height','volume','weight','Product URL']);
-$first = $reader->process([1 => $file]);
-var_dump($first);
+$reader = new Csv('rates/CL131545_product_list.csv',
+    ['id_product','reference','model','manufacturer_name','stock','active','precio','description','description_short','name','category','family','subfamily','comptaible_products','imageURL','EAN','length','width','height','depth','weight','product_url'],
+',',100);
+
+if (!$reader->checkHeader(['id','referencia','Model','Brand','Stock','activo','PVP','Description','Short description','Title','Category','Family','SubFamily','Compatible products','imageURL','EAN','length','width','height','volume','weight','Product URL'])) {
+    die($this->lastError = 'header is not fine');
+}
+$bydemes = new Bydemes();
+$data_file = $reader->read();
 $bydemes_products = Bydemes::getBydemesProducts();
 
