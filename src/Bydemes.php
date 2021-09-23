f0fb6dd93260a2931e30a5054810534ad94e295f
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-23 16:10:49 +0200

    function obtain certain fields by reference

--- src/Bydemes.php
+++ src/Bydemes.php
@@ -6,7 +6,6 @@ namespace OrbitaDigital\OdBydemes;
 
 use Db;
 
-
 class Bydemes
 {
     //available stock -> quantity in ps_stock_available table, requires id_product (from ps_product table)
@@ -18,5 +17,18 @@ class Bydemes
         INNER JOIN `ps_stock_available` sa ON p.id_product = sa.id_product
         INNER JOIN  `ps_product_lang` pl ON p.id_product = pl.id_product 
         INNER JOIN `ps_manufacturer` ma ON p.id_manufacturer = ma.id_manufacturer WHERE p.id_supplier = 1 AND id_lang = 1');
+    }
+    public function processCsv(array $data)
+    {
+        //var_dump($data);
+        //key2 -> header values
+        //value2 ->  row value
+        foreach ($data as $row => $row_values) {
+            $full[] = $this->getRowInfo($row_values['reference']);
+        }
+        return $full;
+    }
+    public function getRowInfo(string $ref){
+        return Db::getInstance()->executeS('SELECT p.reference, p.width, p.height, p.depth AS volume, p.reference FROM `ps_product` p WHERE p.reference = "'.$ref.'"');
     }
 }
